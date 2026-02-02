c68b0e2b1cdae52a82cf6a958d2c3b00045ebd6f
Manuel Olguín Muñoz <mmunoz@cloudflare.com>
2026-02-02 14:28:16 -0800

    Globe key yay

--- keyboards/sofle/keymaps/molguin.v2/keymap.c
+++ keyboards/sofle/keymaps/molguin.v2/keymap.c
@@ -1,7 +1,3 @@
-#include "keycodes.h"
-#include "keymap_us.h"
-#include "process_tap_dance.h"
-#include "quantum_keycodes.h"
 #include QMK_KEYBOARD_H
 
 enum sofle_layers {
@@ -11,25 +7,38 @@ enum sofle_layers {
     _KEYBOARD,
 };
 
+enum custom_keycodes {
+    KC_GLOBE = SAFE_RANGE // fn/Globe key
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KC_GLOBE:
+            host_consumer_send(record->event.pressed ? AC_NEXT_KEYBOARD_LAYOUT_SELECT : 0);
+            return false;
+    }
+    return true;
+}
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-//    ┌─────────────┬───┬───┬──────┬──────┬─────┐                            ┌──────────────┬──────┬───┬─────┬───┬───────────┐
-//    │      1      │ 2 │ 3 │  4   │  5   │  6  │                            │      7       │  8   │ 9 │  0  │ = │   bspc    │
-//    ├─────────────┼───┼───┼──────┼──────┼─────┤                            ├──────────────┼──────┼───┼─────┼───┼───────────┤
-//    │     tab     │ q │ w │  e   │  r   │  t  │                            │      y       │  u   │ i │  o  │ p │     -     │
-//    ├─────────────┼───┼───┼──────┼──────┼─────┤                            ├──────────────┼──────┼───┼─────┼───┼───────────┤
-//    │ LCTL_T(esc) │ a │ s │  d   │  f   │  g  │                            │      h       │  j   │ k │  l  │ ; │ RCTL_T(') │
-//    ├─────────────┼───┼───┼──────┼──────┼─────┼─────────────────┐   ┌──────┼──────────────┼──────┼───┼─────┼───┼───────────┤
-//    │    lsft     │ z │ x │  c   │  v   │  b  │      mute       │   │      │      n       │  m   │ , │  .  │ \ │   rsft    │
-//    └─────────────┴───┼───┼──────┼──────┼─────┼─────────────────┤   ├──────┼──────────────┼──────┼───┼─────┼───┴───────────┘
-//                      │ / │ lalt │ lgui │ spc │ MO(_NAVIGATION) │   │ rgui │ MO(_SYMBOLS) │ ralt │ ` │ ent │
-//                      └───┴──────┴──────┴─────┴─────────────────┘   └──────┴──────────────┴──────┴───┴─────┘
+//    ┌─────────────┬───┬───┬──────┬──────┬─────┐                            ┌──────────────┬──────┬───────┬─────┬───┬───────────┐
+//    │      `      │ 1 │ 2 │  3   │  4   │  5  │                            │      6       │  7   │   8   │  9  │ 0 │   bspc    │
+//    ├─────────────┼───┼───┼──────┼──────┼─────┤                            ├──────────────┼──────┼───────┼─────┼───┼───────────┤
+//    │     tab     │ q │ w │  e   │  r   │  t  │                            │      y       │  u   │   i   │  o  │ p │     -     │
+//    ├─────────────┼───┼───┼──────┼──────┼─────┤                            ├──────────────┼──────┼───────┼─────┼───┼───────────┤
+//    │ LCTL_T(esc) │ a │ s │  d   │  f   │  g  │                            │      h       │  j   │   k   │  l  │ ; │ RCTL_T(') │
+//    ├─────────────┼───┼───┼──────┼──────┼─────┼─────────────────┐   ┌──────┼──────────────┼──────┼───────┼─────┼───┼───────────┤
+//    │    lsft     │ z │ x │  c   │  v   │  b  │      mute       │   │      │      n       │  m   │   ,   │  .  │ \ │   rsft    │
+//    └─────────────┴───┼───┼──────┼──────┼─────┼─────────────────┤   ├──────┼──────────────┼──────┼───────┼─────┼───┴───────────┘
+//                      │ / │ lalt │ lgui │ spc │ MO(_NAVIGATION) │   │ rgui │ MO(_SYMBOLS) │ ralt │ gLOBE │ ent │
+//                      └───┴──────┴──────┴─────┴─────────────────┘   └──────┴──────────────┴──────┴───────┴─────┘
 [_BASE] = LAYOUT(
-  KC_1           , KC_2 , KC_3    , KC_4    , KC_5    , KC_6     ,                                 KC_7         , KC_8    , KC_9    , KC_0     , KC_EQL  , KC_BSPC        ,
-  KC_TAB         , KC_Q , KC_W    , KC_E    , KC_R    , KC_T     ,                                 KC_Y         , KC_U    , KC_I    , KC_O     , KC_P    , KC_MINS        ,
-  LCTL_T(KC_ESC) , KC_A , KC_S    , KC_D    , KC_F    , KC_G     ,                                 KC_H         , KC_J    , KC_K    , KC_L     , KC_SCLN , RCTL_T(KC_QUOT),
-  KC_LSFT        , KC_Z , KC_X    , KC_C    , KC_V    , KC_B     , KC_AUDIO_MUTE   ,     KC_NO   , KC_N         , KC_M    , KC_COMM , KC_DOT   , KC_BSLS , KC_RSFT        ,
-                          KC_SLSH , KC_LALT , KC_LGUI , KC_SPACE , MO(_NAVIGATION) ,     KC_RGUI , MO(_SYMBOLS) , KC_RALT , KC_GRV  , KC_ENTER
+  KC_GRV         , KC_1 , KC_2    , KC_3    , KC_4    , KC_5     ,                                 KC_6         , KC_7    , KC_8     , KC_9     , KC_0    , KC_BSPC        ,
+  KC_TAB         , KC_Q , KC_W    , KC_E    , KC_R    , KC_T     ,                                 KC_Y         , KC_U    , KC_I     , KC_O     , KC_P    , KC_MINS        ,
+  LCTL_T(KC_ESC) , KC_A , KC_S    , KC_D    , KC_F    , KC_G     ,                                 KC_H         , KC_J    , KC_K     , KC_L     , KC_SCLN , RCTL_T(KC_QUOT),
+  KC_LSFT        , KC_Z , KC_X    , KC_C    , KC_V    , KC_B     , KC_AUDIO_MUTE   ,     KC_NO   , KC_N         , KC_M    , KC_COMM  , KC_DOT   , KC_BSLS , KC_RSFT        ,
+                          KC_SLSH , KC_LALT , KC_LGUI , KC_SPACE , MO(_NAVIGATION) ,     KC_RGUI , MO(_SYMBOLS) , KC_RALT , KC_GLOBE , KC_ENTER
 ),
 
 //    ┌──────┬───┬───┬──────┬──────┬─────┐              ┌───┬──────┬───┬───┬───┬──────┐
